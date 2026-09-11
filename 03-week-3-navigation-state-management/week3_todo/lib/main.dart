a17056477ad44e3f206823341d084f04cc219f34
Raditya Zandra Fadhillah <radityazandra4@gmail.com>
2026-09-11 18:56:54 +0700

    feat: add AsyncValue

--- 03-week-3-navigation-state-management/week3_todo/lib/main.dart
+++ 03-week-3-navigation-state-management/week3_todo/lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'pages/todo_page.dart';
+import 'pages/product_page.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Week 3 - ToDo',
-        theme: ThemeData(colorSchemeSeed: Colors.teal, useMaterial3: true),
-        home: const TodoPage(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Praktikum 3',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        useMaterial3: true,
+      ),
+      home: const ProductPage(),
+    );
+  }
 }
